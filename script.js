2559ce1de35f30a726a638348f7ecbea3a63d1f0
Alessandro-CF <achoque.231ds12@istta.edu.pe>
2024-11-14 12:36:12 -0500

    Estructura de la pagina

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const footerText = document.querySelector('footer p');
+    footerText.textContent += " | Gracias por visitar mi página!";
+});
